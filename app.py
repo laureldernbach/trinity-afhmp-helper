29a02244cdc4d47fe592639061972ada1152f71a
Laurel Dernbach <laurel.dernbach@appian.com>
2025-04-16 22:44:16 +0000

    adding some function

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
+from geocodio import GeocodioClient
+
+def geocodio(address):
+    client = GeocodioClient(st.secrets["GEOCODIO_TOKEN"])
+
+    location = client.geocode(address, fields=["census2023"])
+
+    census = location["results"][0]['fields']['census']['2023']
+    tract = census['tract_code']
+    state = census['state_fips']
+    county = census['county_fips'][2:]
+    try:
+        mmsa = census['metro_micro_statistical_area']['area_code']
+    except:
+        mmsa = None
+        print("ERROR: No Metropolitan/Micropolitan Statistical Area found for", location["results"][0]['formatted_address'])
+    return tract, county, mmsa
+
+##################################################
 
 # App title and description
 st.title("Affirmative Fair Housing Marketing Plan (AFHMP) Form Helper")
@@ -13,6 +33,8 @@ search_term = st.text_input("Address:")
 if st.button("Submit"):
     if search_term:
         st.write(f"Gathering demographic data for: {search_term}")
+
+        tract, county, mmsa = geocodio(search_term)
         
         # Simulate data processing (replace with your actual code)
         data = pd.DataFrame({
@@ -22,7 +44,7 @@ if st.button("Submit"):
         
         # Display results
         st.subheader("Demographic Summary")
-        # st.write(f"Found {len(data)} results")
+        st.write(f"Found {tract}, {county}, {mmsa} results")
         
         # Show data table
         st.subheader("Data Table")
